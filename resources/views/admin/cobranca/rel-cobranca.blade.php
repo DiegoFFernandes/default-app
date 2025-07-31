4b7fa0d7b4609a5f23dcb3531f8bf96896f0c7f6
Henrique Candido <henriquecandido8766@gmail.com>
2025-07-31 19:04:23 -0300

    Mudanças em comisao-vendedor e rel cobranca

--- resources/views/admin/cobranca/rel-cobranca.blade.php
+++ resources/views/admin/cobranca/rel-cobranca.blade.php
@@ -100,28 +100,56 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="list-cobranca">
-                            <div id="tabela-dados" class="table table-bordered table-hover text-sm"></div>
-                        </div>
+            <div class="col-12 col-md-12 mb-3">
+                <div class="card card-dark card-outline card-outline-tabs">
+                    <div class="card-header p-0 border-bottom-0">
+                        <ul class="nav nav-tabs" id="tabRelatorio" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link active" id="tab-relatorio-cobranca" data-toggle="pill"
+                                    href="#painel-relatorio-cobranca" role="tab"
+                                    aria-controls="painel-relatorio-cobranca" aria-selected="true">
+                                    Relatório Cobrança
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="tab-inadimplencia" data-toggle="pill"
+                                    href="#painel-inadimplencia" role="tab" aria-controls="painel-inadimplencia"
+                                    aria-selected="false">
+                                    Inadimplência
+                                </a>
+                            </li>
+                        </ul>
                     </div>
-                </div>
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">Contas Mensais</h3>
-                    </div>
                     <div class="card-body">
-                        <div style="height: auto">
-                            <canvas id="contas-mensal-chart" height="50"></canvas>
+                        <div class="tab-content" id="tabContentRelatorio">
+                            <div class="tab-pane fade show active" id="painel-relatorio-cobranca" role="tabpanel"
+                                aria-labelledby="tab-relatorio-cobranca">
+                                <div class="card-body p-2">
+                                    <div class="list-cobranca">
+                                        <div id="tabela-dados" class="table table-bordered table-hover text-sm"></div>
+                                    </div>
+                                    <div class="card">
+                                        <div class="card-header">
+                                            <h3 class="card-title">Contas Mensais</h3>
+                                        </div>
+                                        <div class="card-body">
+                                            <div style="height: auto">
+                                                <canvas id="contas-mensal-chart" height="50"></canvas>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="tab-pane fade" id="painel-inadimplencia" role="tabpanel"
+                                aria-labelledby="tab-inadimplencia">
+                                <!-- Conteúdo  Inadimplência -->
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-
-
         </div>
+
     </section>
     <!-- /.content -->
 @stop
