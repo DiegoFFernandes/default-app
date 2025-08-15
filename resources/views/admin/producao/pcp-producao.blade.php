0914c49e716e7f88b41360d186f37009d63d8206
Henrique Candido <henriquecandido8766@gmail.com>
2025-08-15 18:02:19 -0300

    Tela PCP

--- resources/views/admin/producao/pcp-producao.blade.php
+++ resources/views/admin/producao/pcp-producao.blade.php
@@ -6,31 +6,144 @@
     <section class="content">
         <!-- Small boxes (Stat box) -->
         <div class="row">
-            <div class="col-md-12 col-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">{{ $title_page }}</h3>
-                    </div>
-                    <div class="card-body">
-                        <table id="pneus-lote-pcp" class="table table-bordered table-striped table-font-small compact">
-                        </table>
+            <div class="col-12 col-sm-6 col-md-4 col-lg-2 mb-3">
+                <div class="info-box">
+                    <span class="info-box-icon bg-info"><i class="fas fa-boxes"></i></span>
+                    <div class="info-box-content">
+                        <span class="info-box-text">Qtde Lotes</span>
+                        <span class="info-box-number" id="lotes">0</span>
                     </div>
                 </div>
             </div>
-            {{-- <div class="col-4 col-md-4">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="card-title">Lote PCP</h3>
-                    </div>
-                    <div class="card-body">
-                        <table id="lote-pcp" class="table table-bordered table-striped table-font-small compact">
-                        </table>
+            <div class="col-12 col-sm-6 col-md-4 col-lg-2 mb-3">
+                <div class="info-box">
+                    <span class="info-box-icon bg-info"><i class="far fa-dot-circle"></i></span>
+                    <div class="info-box-content">
+                        <span class="info-box-text">Total Pneus</span>
+                        <span class="info-box-number" id="card-pneus-lote">0</span>
                     </div>
                 </div>
-            </div> --}}
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 col-lg-2 mb-3">
+                <div class="info-box">
+                    <span class="info-box-icon bg-danger"><i class="fas fa-clock"></i></span>
+                    <div class="info-box-content">
+                        <span class="info-box-text">Atrasados</span>
+                        <span class="info-box-number" id="card-pneus-atraso">0</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
+                <div class="info-box">
+                    <span class="info-box-icon bg-warning"><i class="fas fa-hourglass-start"></i></span>
+                    <div class="info-box-content">
+                        <span class="info-box-text">Iniciando</span>
+                        <span class="info-box-number" id="card-pneus-iniciando">0</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
+                <div class="info-box">
+                    <span class="info-box-icon bg-success"><i class="fas fa-check-circle"></i></span>
+                    <div class="info-box-content">
+                        <span class="info-box-text">Finalizados</span>
+                        <span class="info-box-number" id="card-pneus-finalizados">0</span>
+                    </div>
+                </div>
+            </div>
         </div>
+        <!-- Filtro (mudar de acordo com a nescessidade da página) -->
+        <div class="row">
+            <div class="col-12">
+                <div class="card collapsed-card">
+                    <div class="card-header">
+                        <h5 class="card-title">Filtros</h5>
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                <i class="fas fa-plus"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-12 col-md-2 mb-2">
+                                <div class="form-group mb-0">
+                                    <label for="filtro-empresa">Empresa:</label>
+                                    <select id="filtro-empresa" class="form-control mt-1">
+                                        @foreach ($empresa as $emp)
+                                            <option value="{{ $emp->CD_EMPRESA }}"
+                                                {{ $emp->CD_EMPRESA == 1 ? 'selected' : '' }}>
+                                                {{ $emp->NM_EMPRESA }}
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-2 mb-2 d-flex align-items-end">
+                                <button type="button" class="btn btn-primary btn-block" id="submit-seach">Buscar</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 col-12">
+                <div class="card card-dark card-outline card-outline-tabs">
+                    <div class="card-header p-0 border-bottom-0">
+                        <ul class="nav nav-tabs" id="tab-pcp" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link active" id="tab-painelPCP" data-toggle="pill" href="#painel-pcp"
+                                    role="tab" aria-controls="painel-pcp" aria-selected="true">
+                                    Painel PCP
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" id="tab-lotesPCP" data-toggle="pill" href="#painel-lotesPCP"
+                                    role="tab" aria-controls="painel-lotesPCP" aria-selected="false">
+                                    Lotes PCP
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="card-body">
+                        <div class="tab-content" id="tabContentColetas">
+                            <div class="tab-pane fade show active" id="painel-pcp" role="tabpanel"
+                                aria-labelledby="tab-painelPCP">
+                                <div class="table-responsive">
+                                    <table id="pneus-lote-pcp"
+                                        class="table compact table-font-small table-striped table-bordered"
+                                        style="width:100%; font-size: 11px;">
+                                    </table>
+                                </div>
+                            </div>
+                            <div class="tab-pane fade" id="painel-lotesPCP" role="tabpanel"
+                                aria-labelledby="tab-lotesPCP">
+                                <table id="lote-pcp" class="table compact table-font-small table-striped table-bordered"
+                                    style="width:100%; font-size: 11px;">
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </section>
+@stop
+
+@section('css')
+
+    <style>
+        .info-box-text {
+            font-size: 14px;
+        }
+
+        .info-box-number {
+            font-weight: bold;
+            font-size: 18px;
+        }
+    </style>
+
 @stop
 
 @section('js')
@@ -38,7 +151,7 @@
         $(document).ready(function() {
             $('#pneus-lote-pcp').DataTable({
                 processing: true,
-                serverSide: true,
+                serverSide: false,
                 ajax: {
                     url: '{{ route('get-pneus-lote-pcp') }}',
                     type: 'GET'
@@ -92,12 +205,42 @@
                         name: 'DSOBSERVACAO',
                         'title': 'Observação'
                     }
-                ]
+                ],
+                drawCallback: function(settings) {
+                    var api = this.api();
+                    var data = api.rows().data();
+
+                    // variáveis de contagem
+                    let totalPneusLote = data.length;
+                    let totalAtraso = 0;
+                    let totalIniciando = 0;
+                    let totalFinalizados = 0;
+
+                    data.each(function(rowData) {
+                        // formata a sting para nao dar erros
+                        let etapa = rowData.DS_ETAPA ? rowData.DS_ETAPA.trim().toUpperCase() :
+                            '';
+
+                        if (etapa === 'SEM EXAME') {
+                            totalAtraso++;
+                        } else if (etapa === 'EXAME') {
+                            totalIniciando++;
+                        } else if (etapa === 'EXAME FINAL') {
+                            totalFinalizados++;
+                        }
+                    });
+
+                    // atualiza os cards
+                    $('#card-pneus-lote').text(totalPneusLote);
+                    $('#card-pneus-atraso').text(totalAtraso);
+                    $('#card-pneus-iniciando').text(totalIniciando);
+                    $('#card-pneus-finalizados').text(totalFinalizados);
+                }
             });
 
             $('#lote-pcp').DataTable({
                 processing: true,
-                serverSide: true,
+                serverSide: false,
                 ajax: {
                     url: '{{ route('get-lote-pcp') }}',
                     type: 'GET'
@@ -135,7 +278,21 @@
                         name: 'QTDE_SEMEXAME',
                         'title': 'Sem Exame'
                     }
-                ]
+                ],
+                drawCallback: function(settings) {
+                    var api = this.api();
+                    var data = api.rows().data();
+
+                    //busca a quantidade de lotes
+                    let totalLotes = data.length;
+
+                    //atualiza o card de quantidade de lotes
+                    $('#lotes').text(totalLotes);
+                }
+            });
+            $('#submit-seach').on('click', function() {
+                $('#pneus-lote-pcp').DataTable().ajax.reload();
+                $('#lote-pcp').DataTable().ajax.reload();
             });
         });
     </script>
