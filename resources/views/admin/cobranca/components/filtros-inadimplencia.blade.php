c1dde7a9630a12bd57c689ddb63b7b9e5e5265e9
Diego Ferreira <ti.campina@ivorecap.com.br>
2026-04-20 17:58:48 -0300

    Labels Relatorio de cobranca

--- resources/views/admin/cobranca/components/filtros-inadimplencia.blade.php
+++ resources/views/admin/cobranca/components/filtros-inadimplencia.blade.php
@@ -11,12 +11,13 @@
     <div class="card-body">
         <div class="row">
             <div class="col-md-4 mb-2">
-                {{-- <input id="filtro-nome" type="text" class="form-control form-control-sm" placeholder="Filtrar por Cliente"> --}}
+                <label for="{{ $pessoa }}" class="form-label small">Pessoa</label>
                 <select name='pessoa' class="form-control form-control-sm" id="{{ $pessoa }}"
                     style="width: 100%">
                 </select>
             </div>
             <div class="col-md-4 mb-2">
+                <label for="{{ $filtro_gerente }}" class="form-label small">Gerente Comercial</label>
                 <select name="gerente" id="{{ $filtro_gerente }}" class="form-control form-control-sm"
                     style="width: 100%">
                     <option value="0">Todos Gerentes</option>
@@ -26,25 +27,30 @@
                 </select>
             </div>
             <div class="col-md-4 mb-2">
+                <label for="{{ $filtro_supervisor }}" class="form-label small">Supervisor</label>
                 <input id="{{ $filtro_supervisor }}" type="text" class="form-control form-control-sm"
                     placeholder="Filtrar por Supervisor">
             </div>
             <div class="col-md-4 mb-2">
+                <label for="{{ $filtro_vendedor }}" class="form-label small">Vendedor</label>
                 <input id="{{ $filtro_vendedor }}" type="text" class="form-control form-control-sm"
                     placeholder="Filtrar por Vendedor">
             </div>
             <div class="col-md-3 mb-2">
+                <label for="{{ $filtro_cnpj }}" class="form-label small">CNPJ</label>
                 <input id="{{ $filtro_cnpj }}" type="text" class="form-control form-control-sm"
                     placeholder="Filtrar por CNPJ">
             </div>
             <div class="col-md-3 mb-2">
+                <label for="{{ $daterange }}" class="form-label small">Período</label>
                 <input id="{{ $daterange }}" type="text" class="form-control form-control-sm"
                     placeholder="{{ $placeholderDatarange }}">
             </div>
             <div class="col-md-2 mb-2">
+                <label for="{{ $filtro_cartorio }}" class="form-label small">Cartório/Protesto</label>
                 <select name="cartorio" id="{{ $filtro_cartorio }}" class="form-control form-control-sm"
                     style="width: 100%">
-                    <option value="0">Todos</option>
+                    <option value="0">Nenhum</option>
                     <option value="1">Com Cartorio</option>
                     <option value="2">Sem Cartorio</option>
                 </select>
